d26a85fb9171928a255ccc56688051c8b5e29276
Denis Leeroi Villamiel <denis_leeroi_villamiel@dlsu.edu.ph>
2023-07-24 12:47:27 +0800

    video 8 stuff

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -9,7 +9,7 @@ router.get('/admin', async (req, res) => {
     try{
         const locals = {
             title: "MVX - Admin", //according to the 2nd video, 5:43
-            description: "Landing page for MVX"
+            description: "Admin page for MVX"
         }
 
         res.render('admin/login', {locals, layout: adminLayout});
@@ -20,13 +20,22 @@ router.get('/admin', async (req, res) => {
 
 
 
-router.post('/admin', async (req, res) => {
+router.post('/register', async (req, res) => {
     try{
 
         const {username, password} = req.body;
-        console.log(req.body);
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-        res.redirect('/admin', {locals, layout: adminLayout});
+        try {
+            const user = await User.create({ username, password: hashedPassword });
+            res.status(201).json({ message: 'User Created', user}) //for testing purposes
+        } catch (error) {
+            if(error.code === 11000){
+                res.status(409).json({message: 'User already in use'});
+            }
+            res.status(500).json({message: 'Internal server error'});
+        }
+
     } catch (error){
         console.log(error);
     }
